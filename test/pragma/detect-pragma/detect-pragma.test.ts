0e880be49d2df8b35dcdc1e9ecf87123141a78ec
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2019-06-22 22:43:03 +0200

    Add Test for detecting pragma

---
+++ test/pragma/detect-pragma/detect-pragma.test.ts
@@ -0,0 +1,28 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { parsers } from './../../../src/index';
+import { Parser } from 'prettier';
+
+// ts-jest needs the exclamation mark, so it does not have the impression that the variables are undefined
+const pugParser: Parser = parsers!.pug;
+
+describe('Pragma', () => {
+	test('should detect pragma @prettier', async () => {
+		const code: string = readFileSync(resolve(__dirname, 'pragma-prettier.pug'), 'utf8');
+		const actual: boolean = pugParser.hasPragma!(code);
+
+		expect(actual).toBeTruthy();
+	});
+	test('should detect pragma @format', async () => {
+		const code: string = readFileSync(resolve(__dirname, 'pragma-format.pug'), 'utf8');
+		const actual: boolean = pugParser.hasPragma!(code);
+
+		expect(actual).toBeTruthy();
+	});
+	test('should detect no pragma ', async () => {
+		const code: string = readFileSync(resolve(__dirname, 'no-pragma.pug'), 'utf8');
+		const actual: boolean = pugParser.hasPragma!(code);
+
+		expect(actual).toBeFalsy();
+	});
+});
