c21b0e1194aa907c949bfce9f05f6f140a70b1da
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2019-06-22 18:01:48 +0200

    Format test

--- test/quotes/single-to-double/single-to-double-quotes.test.ts
+++ test/quotes/single-to-double/single-to-double-quotes.test.ts
@@ -5,12 +5,9 @@ import { plugin } from './../../../src/index';
 
 describe('Quotes', () => {
 	test('should format single to double quotes', () => {
-		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf-8');
-		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf-8');
-		const actual: string = format(code, {
-			parser: 'pug' as any,
-			plugins: [plugin]
-		});
+		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
+		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
+		const actual: string = format(code, { parser: 'pug' as any, plugins: [plugin] });
 
 		expect(actual).toBe(expected);
 	});
