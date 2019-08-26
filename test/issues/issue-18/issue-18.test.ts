0a9f8484e98376774cb758c77d60a8c84691b201
Christopher Quadflieg <chrissi92@hotmail.de>
2019-08-26 21:41:02 +0200

    Rename test

--- test/issues/issue-18/issue-18.test.ts
+++ test/issues/issue-18/issue-18.test.ts
@@ -4,7 +4,7 @@ import { format } from 'prettier';
 import { plugin } from './../../../src/index';
 
 describe('Issues', () => {
-	test('should handle mixins', () => {
+	test('should handle include', () => {
 		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
 		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
 		const actual: string = format(code, { parser: 'pug' as any, plugins: [plugin] });
