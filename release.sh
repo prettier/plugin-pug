2f27b71fc655e93137ee60d01121dd031282ffb3
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2019-11-15 18:10:17 +0100

    Add release script

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -x
+
+# Cleanup
+rm -Rf coverage
+rm -Rf dist
+rm -Rf node_modules
+rm junit.xml
+rm yarn.lock
+
+# Prepare
+yarn install
+yarn lint
+yarn test --silent
+yarn audit
+yarn build
+
+# Publish
+npm publish --dry-run
+
+set +x
+
+echo
+echo "If everything looks okay, use 'npm publish --access public'"
+echo
+echo "Remember to create a tag"
+echo "Remember to increase the version number"
+echo
