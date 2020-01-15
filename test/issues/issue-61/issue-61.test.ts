c8bd5b6561174edf3cccc38214f2e7d9abbee85b
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-01-15 13:03:51 +0100

    Add test for issue 61

---
+++ test/issues/issue-61/issue-61.test.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { format } from 'prettier';
+import { plugin } from './../../../src/index';
+
+describe('Issues', () => {
+	test('should not wrap chained data-bindings', () => {
+		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
+		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
+		const actual: string = format(code, {
+			parser: 'pug' as any,
+			plugins: [plugin],
+
+			semi: false,
+			singleQuote: true
+		});
+
+		expect(actual).toBe(expected);
+	});
+});
