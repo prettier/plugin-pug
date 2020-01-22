a37c8d9d392d1dd06b6a18b91005f67e3e3178a3
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-01-22 18:50:34 +0100

    Optimize functions and replace regex

--- src/utils/angular.ts
+++ src/utils/angular.ts
@@ -1,11 +1,24 @@
 export function isAngularExpression(name: string): boolean {
-	return /^(\(.*\)|\[.*\])$/.test(name);
+	return (
+		name.length >= 3 &&
+		// eslint-disable-next-line @typescript-eslint/prefer-string-starts-ends-with
+		((name[0] === '(' && name[name.length - 1] === ')') || (name[0] === '[' && name[name.length - 1] === ']'))
+	);
 }
 
 export function isAngularDirective(name: string): boolean {
-	return /^\*.*$/.test(name);
+	// eslint-disable-next-line @typescript-eslint/prefer-string-starts-ends-with
+	return name.length >= 2 && name[0] === '*';
 }
 
 export function isAngularInterpolation(val: string): boolean {
-	return /^(["']{{)(.*)(}}["'])$/.test(val);
+	return (
+		val.length >= 5 &&
+		// eslint-disable-next-line @typescript-eslint/prefer-string-starts-ends-with
+		((val[0] === '"' && val[val.length - 1] === '"') || (val[0] === "'" && val[val.length - 1] === "'")) &&
+		val[1] === '{' &&
+		val[2] === '{' &&
+		val[val.length - 2] === '}' &&
+		val[val.length - 3] === '}'
+	);
 }
--- src/utils/vue.ts
+++ src/utils/vue.ts
@@ -1,3 +1,3 @@
 export function isVueExpression(name: string): boolean {
-	return /^((v-bind|v-on|v-slot)?:|v-model|v-on|@).*/.test(name);
+	return /^((v-bind|v-slot)?:|v-model|v-on|@).*/.test(name);
 }
