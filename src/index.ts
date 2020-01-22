d972341a608e1b6746a7adad11a60227d7d1181f
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-01-22 18:35:06 +0100

    Move utility functions into own file

--- src/index.ts
+++ src/index.ts
@@ -9,77 +9,13 @@ import {
 	PugParserOptions,
 	resolveAttributeSeparatorOption
 } from './options';
+import { formatText, previousNormalAttributeToken, printIndent, unwrapLineFeeds } from './utils/common';
 
 const { makeString } = util;
 
 const logger: Logger = createLogger(console);
 if (process.env.NODE_ENV === 'test') {
 	logger.setLogLevel(LogLevel.DEBUG);
-}
-
-function previousNormalAttributeToken(tokens: Token[], index: number): AttributeToken | undefined {
-	for (let i: number = index - 1; i > 0; i--) {
-		const token: Token = tokens[i];
-		if (token.type === 'start-attributes') {
-			return;
-		}
-		if (token.type === 'attribute') {
-			if (token.name !== 'class' && token.name !== 'id') {
-				return token;
-			}
-		}
-	}
-	return;
-}
-
-function printIndent(previousToken: Token, indent: string, indentLevel: number): string {
-	switch (previousToken?.type) {
-		case 'newline':
-		case 'outdent':
-			return indent.repeat(indentLevel);
-		case 'indent':
-			return indent;
-	}
-	return '';
-}
-
-function formatText(text: string, singleQuote: boolean): string {
-	let result: string = '';
-	while (text) {
-		const start = text.indexOf('{{');
-		if (start !== -1) {
-			result += text.slice(0, start);
-			text = text.substring(start + 2);
-			const end = text.indexOf('}}');
-			if (end !== -1) {
-				let code = text.slice(0, end);
-				code = code.trim();
-				code = format(code, { parser: 'babel', singleQuote: !singleQuote, printWidth: 9000 });
-				if (code.endsWith(';\n')) {
-					code = code.slice(0, -2);
-				}
-				result += `{{ ${code} }}`;
-				text = text.slice(end + 2);
-			} else {
-				result += '{{';
-				result += text;
-				text = '';
-			}
-		} else {
-			result += text;
-			text = '';
-		}
-	}
-	return result;
-}
-
-function unwrapLineFeeds(value: string): string {
-	return value.includes('\n')
-		? value
-				.split('\n')
-				.map((part) => part.trim())
-				.join('')
-		: value;
 }
 
 export const plugin: Plugin = {
---
+++ src/utils/common.ts
@@ -0,0 +1,67 @@
+import { format } from 'prettier';
+import { AttributeToken, Token } from 'pug-lexer';
+
+export function previousNormalAttributeToken(tokens: Token[], index: number): AttributeToken | undefined {
+	for (let i: number = index - 1; i > 0; i--) {
+		const token: Token = tokens[i];
+		if (token.type === 'start-attributes') {
+			return;
+		}
+		if (token.type === 'attribute') {
+			if (token.name !== 'class' && token.name !== 'id') {
+				return token;
+			}
+		}
+	}
+	return;
+}
+
+export function printIndent(previousToken: Token, indent: string, indentLevel: number): string {
+	switch (previousToken?.type) {
+		case 'newline':
+		case 'outdent':
+			return indent.repeat(indentLevel);
+		case 'indent':
+			return indent;
+	}
+	return '';
+}
+
+export function formatText(text: string, singleQuote: boolean): string {
+	let result: string = '';
+	while (text) {
+		const start = text.indexOf('{{');
+		if (start !== -1) {
+			result += text.slice(0, start);
+			text = text.substring(start + 2);
+			const end = text.indexOf('}}');
+			if (end !== -1) {
+				let code = text.slice(0, end);
+				code = code.trim();
+				code = format(code, { parser: 'babel', singleQuote: !singleQuote, printWidth: 9000 });
+				if (code.endsWith(';\n')) {
+					code = code.slice(0, -2);
+				}
+				result += `{{ ${code} }}`;
+				text = text.slice(end + 2);
+			} else {
+				result += '{{';
+				result += text;
+				text = '';
+			}
+		} else {
+			result += text;
+			text = '';
+		}
+	}
+	return result;
+}
+
+export function unwrapLineFeeds(value: string): string {
+	return value.includes('\n')
+		? value
+				.split('\n')
+				.map((part) => part.trim())
+				.join('')
+		: value;
+}
