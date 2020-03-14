0e11d47ddae9fc8d63ebdd883bb2127d3ccf8880
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-03-14 10:09:17 +0100

    Add documentation

--- src/utils/vue.ts
+++ src/utils/vue.ts
@@ -1,3 +1,25 @@
+/**
+ * Indicates whether the attribute name is a Vue expression
+ *
+ * ---
+ *
+ * Example expression:
+ * ```
+ * v-btn(@click="doSomething") Do Something
+ * ```
+ *
+ * In this case `name` is `@click`
+ *
+ * ---
+ *
+ * Checks for: `v-bind`, `v-slot`, `v-model` and `v-on`.
+ *
+ * Also shorthands like `:*` and `@*` are checked.
+ *
+ * ---
+ *
+ * @param name Name of tag attribute
+ */
 export function isVueExpression(name: string): boolean {
 	return /^((v-bind|v-slot)?:|v-model|v-on|@).*/.test(name);
 }
