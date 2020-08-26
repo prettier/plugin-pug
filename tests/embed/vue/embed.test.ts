cd8a64e3ede674210593cbbf79d029c76a377d57
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-08-26 15:24:47 +0200

    Enable embed vue empty template test

--- tests/embed/vue/embed.test.ts
+++ tests/embed/vue/embed.test.ts
@@ -20,8 +20,7 @@ describe('Embedded', () => {
 		expect(actual).toBe(expected);
 	});
 
-	// Enable when https://github.com/prettier/prettier/issues/9049 is resolved
-	test.skip('should format when embedded in vue empty template', () => {
+	test('should format when embedded in vue empty template', () => {
 		const expected: string = readFileSync(resolve(__dirname, 'formatted-empty-template.vue'), 'utf8');
 		const code: string = readFileSync(resolve(__dirname, 'unformatted-empty-template.vue'), 'utf8');
 		const actual: string = format(code, { parser: 'vue', plugins: [plugin] });
