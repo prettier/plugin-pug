40669bd4749d7f809abbde7d3c5820f3cd3fbf45
Jürg Lehni <juerg@scratchdisk.com>
2020-09-30 00:22:27 +0200

    Fix spelling mistake

--- tests/options/pugMaxSingleLineAttributes/2/2.test.ts
+++ tests/options/pugMaxSingleLineAttributes/2/2.test.ts
@@ -5,7 +5,7 @@ import { plugin } from './../../../../src/index';
 
 describe('Options', () => {
 	describe('pugMaxSingleLineAttributes', () => {
-		test('should not allow more than two attribute on the first line of an element', () => {
+		test('should not allow more than two attributes on the first line of an element', () => {
 			const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
 			const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
 			const actual: string = format(code, {
