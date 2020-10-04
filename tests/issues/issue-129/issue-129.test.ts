2049c123e118c55e75c933956795b767e5e47688
Shinigami92 <chrissi92@hotmail.de>
2020-10-04 17:22:23 +0200

    Fix wording

--- tests/issues/issue-129/issue-129.test.ts
+++ tests/issues/issue-129/issue-129.test.ts
@@ -4,7 +4,7 @@ import { format } from 'prettier';
 import { plugin } from './../../../src/index';
 
 describe('Issues', () => {
-	test("should vue's v-for with for...of style", () => {
+	test("should handle vue's v-for with for...of style", () => {
 		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
 		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
 		const actual: string = format(code, { parser: 'pug' as any, plugins: [plugin] });
