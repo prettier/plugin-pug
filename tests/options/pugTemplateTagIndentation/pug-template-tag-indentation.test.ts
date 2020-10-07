80e1415517c280b44f90c445b31a58521231cf5c
Shinigami92 <chrissi92@hotmail.de>
2020-10-07 15:36:24 +0200

    Add test for issue 140

---
+++ tests/options/pugTemplateTagIndentation/pug-template-tag-indentation.test.ts
@@ -0,0 +1,42 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { format } from 'prettier';
+import { plugin } from './../../../src/index';
+
+describe('Options', () => {
+	describe('pugTemplateTagIndentation', () => {
+		test('should indent', () => {
+			const expected: string = readFileSync(resolve(__dirname, 'indented.vue'), 'utf8');
+			const code: string = readFileSync(resolve(__dirname, 'not-indented.vue'), 'utf8');
+			const actual: string = format(code, {
+				parser: 'vue',
+				plugins: [plugin],
+				// @ts-expect-error
+				pugTemplateTagIndentation: true
+			});
+
+			expect(actual).toBe(expected);
+		});
+
+		test('should not indent', () => {
+			const expected: string = readFileSync(resolve(__dirname, 'not-indented.vue'), 'utf8');
+			const code: string = readFileSync(resolve(__dirname, 'indented.vue'), 'utf8');
+			const actual: string = format(code, {
+				parser: 'vue',
+				plugins: [plugin],
+				// @ts-expect-error
+				pugTemplateTagIndentation: false
+			});
+
+			expect(actual).toBe(expected);
+		});
+
+		test('should not indent by default', () => {
+			const expected: string = readFileSync(resolve(__dirname, 'not-indented.vue'), 'utf8');
+			const code: string = readFileSync(resolve(__dirname, 'indented.vue'), 'utf8');
+			const actual: string = format(code, { parser: 'vue', plugins: [plugin] });
+
+			expect(actual).toBe(expected);
+		});
+	});
+});
