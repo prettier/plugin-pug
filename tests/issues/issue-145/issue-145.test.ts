6d751ed220b521bfadf1e0956b286239884fd169
Shinigami92 <chrissi92@hotmail.de>
2020-10-27 14:13:37 +0100

    Extend test for issue 145

--- tests/issues/issue-145/issue-145.test.ts
+++ tests/issues/issue-145/issue-145.test.ts
@@ -11,4 +11,17 @@ describe('Issues', () => {
 
 		expect(actual).toBe(expected);
 	});
+
+	test('should preserve multi-root nodes with pugSingleFileComponentIndentation', () => {
+		const expected: string = readFileSync(resolve(__dirname, 'formatted.vue'), 'utf8');
+		const code: string = readFileSync(resolve(__dirname, 'unformatted.vue'), 'utf8');
+		const actual: string = format(code, {
+			parser: 'pug',
+			plugins: [plugin],
+			// @ts-expect-error
+			pugSingleFileComponentIndentation: true
+		});
+
+		expect(actual).toBe(expected);
+	});
 });
