a32774613daa507d6431b791f8e8077efef5c800
Shinigami <chrissi92@hotmail.de>
2020-11-24 21:17:58 +0100

    Add test for issue 156 (#157)

---
+++ tests/issues/issue-156/issue-156.test.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { format } from 'prettier';
+import { plugin } from './../../../src/index';
+
+describe('Issues', () => {
+	test('should handle multiline inline script', () => {
+		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
+		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
+		const actual: string = format(code, {
+			parser: 'pug',
+			plugins: [plugin],
+			printWidth: 120,
+			endOfLine: 'auto'
+		});
+
+		expect(actual).toBe(expected);
+	});
+});
