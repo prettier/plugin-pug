213f34cb2e6d918b24a04b7bcc2fbc35427da784
Antonio Eduardo <antonio@emiolo.com>
2020-11-28 13:18:03 -0300

    Switch lines

--- src/options/converge.ts
+++ src/options/converge.ts
@@ -31,8 +31,8 @@ export function convergeOptions(options: ParserOptions & PugParserOptions): PugP
 		pugSortAttributesEnd: options.pugSortAttributesEnd,
 		pugWrapAttributesThreshold: options.pugWrapAttributesThreshold,
 		pugWrapAttributesPattern: options.pugWrapAttributesPattern,
-		pugIdNotation: options.pugIdNotation,
 		pugClassNotation: options.pugClassNotation,
+		pugIdNotation: options.pugIdNotation,
 		pugEmptyAttributes: options.pugEmptyAttributes,
 		pugEmptyAttributesForceQuotes: options.pugEmptyAttributesForceQuotes,
 		pugSingleFileComponentIndentation: options.pugSingleFileComponentIndentation && options.embeddedInHtml
