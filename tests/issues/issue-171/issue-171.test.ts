a4465e895e7bcc36506149d9d50ed1bd9a885498
Shinigami92 <chrissi92@hotmail.de>
2021-01-08 14:07:45 +0100

    Add test name

--- tests/issues/issue-171/issue-171.test.ts
+++ tests/issues/issue-171/issue-171.test.ts
@@ -4,7 +4,7 @@ import { format } from 'prettier';
 import { plugin } from './../../../src/index';
 
 describe('Issues', () => {
-	test('should ...', () => {
+	test('should not indent blank lines with an indent in inline syntax', () => {
 		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
 		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
 		const actual: string = format(code, { parser: 'pug', plugins: [plugin] });
