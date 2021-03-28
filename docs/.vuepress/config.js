3b7fe570b7b415f6b416066ca08c273838b41135
Shinigami92 <chrissi92@hotmail.de>
2021-03-28 22:25:04 +0200

    Use vuepress-theme-yuu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,8 +4,13 @@ module.exports = {
 	description: '@prettier/plugin-pug documentation',
 	base: process.env.NODE_ENV === 'production' ? '/plugin-pug/' : '/',
 
-	// theme: 'yuu',
+	theme: 'yuu',
 	themeConfig: {
+		yuu: {
+			defaultDarkTheme: true,
+			defaultColorTheme: 'blue'
+		},
+
 		repo: 'prettier/plugin-pug',
 		logo:
 			'https://camo.githubusercontent.com/f89d53e813140369c509e67424833cdfc3f76dcf997d4b3b0a1174ec12304600/68747470733a2f2f63646e2e7261776769742e636f6d2f70726574746965722f70726574746965722d6c6f676f2f6d61737465722f696d616765732f70726574746965722d69636f6e2d6c696768742e737667',
