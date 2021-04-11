27fb25a30021a91348b26c2f9774b9b025e3cc5b
Shinigami92 <chrissi92@hotmail.de>
2021-04-11 13:27:23 +0200

    Remove comment

--- tests/formatText/warnings/warnings.test.ts
+++ tests/formatText/warnings/warnings.test.ts
@@ -27,7 +27,6 @@ function getFormatWarnings(calls: Call[], logPrefix: string = DEFAULT_LOG_PREFIX
 describe('Frameworks', () => {
 	describe('Angular', () => {
 		test('should warn for missing parentheses', () => {
-			// this test is not completed yet
 			const consoleSpy: jest.SpyInstance<void, Call> = jest.spyOn(console, 'warn');
 			const code: string = readFileSync(resolve(__dirname, 'missing-expected-close-parentheses.pug'), 'utf8');
 			format(code, {
