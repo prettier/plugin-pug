99e548c43298f3507dd06df1e8782993392f3e37
John Manuel <bitencode@johnmanuel.org>
2021-06-27 16:33:34 -0700

    better unit tests

--- tests/options/pugExplicitDiv/pug-explicit-div.test.ts
+++ tests/options/pugExplicitDiv/pug-explicit-div.test.ts
@@ -1,29 +1,46 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 import { format } from 'prettier';
 import { plugin } from '../../../src/index';
 
 describe('Options', () => {
 	describe('pugExplicitDiv', () => {
-		test('should handle pugExplicitDiv:true', () => {
-			const expected: string = 'div.my-class\n';
-			const code: string = 'div.my-class\n';
+		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
+		const commonOpts = {
+			parser: 'pug',
+			plugins: [plugin],
+			// use this common options in all tests to force specific wrapping
+			pugAttributeSeparator: 'none',
+			pugPrintWidth: 80,
+			// pugWrapAttributesThreshold: 100,
+		}
+
+		test('should handle unspecified pugExplicitDiv', () => {
+			const expected: string = readFileSync(resolve(__dirname, 'formatted-implicit-div.pug'), 'utf8');
 			const actual: string = format(code, {
-				parser: 'pug',
-				plugins: [plugin],
-				// @ts-expect-error
-				pugExplicitDiv: true
+				...commonOpts,
 			});
 
 			expect(actual).toBe(expected);
 		});
 
 		test('should handle pugExplicitDiv:false', () => {
-			const expected: string = '.my-class\n';
-			const code: string = 'div.my-class\n';
+			const expected: string = readFileSync(resolve(__dirname, 'formatted-implicit-div.pug'), 'utf8');
 			const actual: string = format(code, {
-				parser: 'pug',
-				plugins: [plugin],
 				// @ts-expect-error
-				pubExplicitDiv: false
+				pugExplicitDiv: false,
+				...commonOpts,
+			});
+
+			expect(actual).toBe(expected);
+		});
+
+		test('should handle pugExplicitDiv:true', () => {
+			const expected: string = readFileSync(resolve(__dirname, 'formatted-explicit-div.pug'), 'utf8');
+			const actual: string = format(code, {
+				// @ts-expect-error
+				pugExplicitDiv: true,
+				...commonOpts,
 			});
 
 			expect(actual).toBe(expected);
