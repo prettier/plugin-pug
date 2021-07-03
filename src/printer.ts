83d35a7e4b56007b2a49ddf4c8e38cba8ddda113
Jason Allan <code@jasonderrythomas.com>
2021-07-03 22:20:06 +0100

    Handle non-JS script content

--- src/printer.ts
+++ src/printer.ts
@@ -82,8 +82,10 @@ import {
 	makeString,
 	previousNormalAttributeToken,
 	previousTagToken,
+	previousTypeAttributeToken,
 	unwrapLineFeeds
 } from './utils/common';
+import { getScriptParserName } from './utils/script-mime-types';
 import { isSvelteInterpolation } from './utils/svelte';
 import {
 	isVueEventBinding,
@@ -1281,7 +1283,7 @@ export class PugPrinter {
 			let parser: BuiltInParserName | undefined;
 			switch (lastTagToken?.val) {
 				case 'script':
-					parser = 'babel';
+					parser = getScriptParserName(previousTypeAttributeToken(this.tokens, this.currentIndex));
 					break;
 				case 'style':
 					parser = 'css';
