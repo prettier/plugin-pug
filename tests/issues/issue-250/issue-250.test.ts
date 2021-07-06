0aa1212cdda594793de2bb3eebe06950adfbb5b9
John Manuel <bitencode@johnmanuel.org>
2021-07-06 08:20:20 -0700

    fix unit test description and test case
    - fix copy/paste error on test description
    - fix formatted/unformatted test files:
    - unquoted pug variable assignment to Vue prop should not be quoted

--- tests/issues/issue-250/issue-250.test.ts
+++ tests/issues/issue-250/issue-250.test.ts
@@ -4,7 +4,7 @@ import { format } from 'prettier';
 import { plugin } from './../../../src/index';
 
 describe('Issues', () => {
-	test('should handle spread destructuring in Vue syntax', () => {
+	test('should not quote pug variables assigned to Vue props', () => {
 		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
 		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
 		const actual: string = format(code, { parser: 'pug', plugins: [plugin] });
