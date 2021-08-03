de9b91ba7dd0e01a261f3dfcb710168221a017d5
Shinigami92 <chrissi92@hotmail.de>
2021-08-03 18:33:47 +0200

    Improve test description

--- tests/issues/issue-261/issue-261.test.ts
+++ tests/issues/issue-261/issue-261.test.ts
@@ -4,7 +4,7 @@ import { format } from 'prettier';
 import { plugin } from './../../../src/index';
 
 describe('Issues', () => {
-	test('should preserve backticks', () => {
+	test('should preserve backticks for class and id attributes', () => {
 		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
 		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
 		const actual: string = format(code, { parser: 'pug', plugins: [plugin] });
