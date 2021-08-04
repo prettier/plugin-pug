998a192e7d1f5ba1af4f98aacf0448dbcc26364a
Jason Allan <code@jasonderrythomas.com>
2021-08-04 16:54:40 +0100

    Add `.vscode/launch.json` for debugging (#271)

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	testEnvironment: 'node',
 	rootDir: '.',
 	testMatch: ['<rootDir>/tests/**/*.test.ts'],
-	moduleFileExtensions: ['ts', 'js'],
+	moduleFileExtensions: ['ts', 'js', 'pug'],
 	reporters: ['default', 'jest-junit'],
 	collectCoverage: true,
 	collectCoverageFrom: ['src/**/*.ts', '!src/logger.ts']
