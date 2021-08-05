2503fcf0b8eeefc8b01613393153b03a8157295b
Shinigami92 <chrissi92@hotmail.de>
2021-08-05 19:44:08 +0200

    Use pugTabWidth for calculating line length

--- tests/issues/issue-268/issue-268.test.ts
+++ tests/issues/issue-268/issue-268.test.ts
@@ -4,14 +4,14 @@ import { format } from 'prettier';
 import { plugin } from './../../../src/index';
 
 describe('Issues', () => {
-	test('should correctly format when using useTabs', () => {
+	test('should consider tabWidth when using useTabs', () => {
 		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
 		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
 		const actual: string = format(code, {
 			parser: 'pug',
 			plugins: [plugin],
 			useTabs: true,
-			tabWidth: 8 // Just for example
+			tabWidth: 8
 		});
 
 		expect(actual).toBe(expected);
