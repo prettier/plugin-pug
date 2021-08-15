0fce0d3bb4207fb397c9dcc874fc785b89d22226
Shinigami92 <chrissi92@hotmail.de>
2021-08-15 14:26:52 +0200

    Remove unnecessary code
    Co-authored-by: ST-DDT <ST-DDT@gmx.de>

--- src/printer.ts
+++ src/printer.ts
@@ -1406,10 +1406,6 @@ export class PugPrinter {
 				tok = this.tokens[index - 1];
 				if (tok?.type === 'text' && tok.val === '') {
 					result += '\n';
-					const nextToken: Token | undefined = this.tokens[index + 1];
-					if (nextToken?.type !== 'outdent' && nextToken?.type !== 'newline') {
-						result += this.indentString.repeat(this.indentLevel);
-					}
 				}
 
 				this.currentIndex = index - 1;
