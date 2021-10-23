4ca7b148c1dbdfa4ce81c12e86696b410152bf21
Shinigami <chrissi92@hotmail.de>
2021-10-23 18:28:24 +0200

    Fix ignore escaping chars in vue prop (#304)

---
+++ tests/issues/issue-303/issue-303.test.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { format } from 'prettier';
+import { plugin } from './../../../src/index';
+
+describe('Issues', () => {
+	test('should ignore escaping chars in vue prop', () => {
+		const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
+		const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
+		const actual: string = format(code, {
+			parser: 'pug',
+			plugins: [plugin],
+			singleQuote: true,
+			// @ts-expect-error
+			pugSingleQuote: false
+		});
+
+		expect(actual).toBe(expected);
+	});
+});
