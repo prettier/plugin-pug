4752d2c8dac6b3701caec93f18862028965f4d95
Taylor Schley <taylorschley@me.com>
2021-11-24 16:24:18 -0600

    test: add other tests

---
+++ tests/options/singleQuote/single-false-pug-true/single-false-pug-true.test.ts
@@ -0,0 +1,22 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { format } from 'prettier';
+import { plugin } from './../../../../src/index';
+
+describe('Options', () => {
+	describe('singleQuote', () => {
+		test('should handle singleQuote:false + pugSingleQuote:true', () => {
+			const expected: string = readFileSync(resolve(__dirname, 'formatted.pug'), 'utf8');
+			const code: string = readFileSync(resolve(__dirname, 'unformatted.pug'), 'utf8');
+			const actual: string = format(code, {
+				parser: 'pug',
+				plugins: [plugin],
+
+				singleQuote: false,
+				pugSingleQuote: true
+			});
+
+			expect(actual).toBe(expected);
+		});
+	});
+});
