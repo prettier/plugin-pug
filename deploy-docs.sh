0e39b5a9b538f4c75e8c92e5ffe686e386204041
Shinigami92 <chrissi92@hotmail.de>
2021-12-17 14:20:10 +0100

    Switch to Vitepress

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd docs/.vitepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
