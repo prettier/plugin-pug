9481750100637967f3e0c6395a7ffdb5c2ff166d
Shinigami92 <chrissi92@hotmail.de>
2022-01-19 16:41:44 +0100

    Add test for issue 329

---
+++ tests/issues/issue-329/issue-329.test.ts
@@ -0,0 +1,27 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { format } from 'prettier';
+import { describe, expect, test } from 'vitest';
+import { plugin } from '../../../src/index';
+
+describe('Issues', () => {
+  test('should handle nested angular translations', () => {
+    const expected: string = readFileSync(
+      resolve(__dirname, 'formatted.pug'),
+      'utf8',
+    );
+    const code: string = readFileSync(
+      resolve(__dirname, 'unformatted.pug'),
+      'utf8',
+    );
+    const actual: string = format(code, {
+      parser: 'pug',
+      plugins: [plugin],
+
+      printWidth: 120,
+      pugFramework: 'angular',
+    });
+
+    expect(actual).toBe(expected);
+  });
+});
