057adc4212a1fc37f58ea8452d0cd97d3d29850c
Shinigami92 <chrissi92@hotmail.de>
2022-06-04 14:58:33 +0200

    Upgrade VitePress docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,23 +1,34 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  lang: 'en-US',
-  title: 'Documentation',
+  title: 'Prettier Plugin Pug',
   description: '@prettier/plugin-pug documentation',
-  base: process.env.NODE_ENV === 'production' ? '/plugin-pug/' : '/',
+
   head: [['link', { rel: 'icon', href: `/icon.png` }]],
 
+  base: process.env.NODE_ENV === 'production' ? '/plugin-pug/' : '/',
+
   themeConfig: {
-    repo: 'prettier/plugin-pug',
-    docsDir: 'docs',
-    docsBranch: 'main',
     logo: 'https://camo.githubusercontent.com/f89d53e813140369c509e67424833cdfc3f76dcf997d4b3b0a1174ec12304600/68747470733a2f2f63646e2e7261776769742e636f6d2f70726574746965722f70726574746965722d6c6f676f2f6d61737465722f696d616765732f70726574746965722d69636f6e2d6c696768742e737667',
-    editLinks: true,
+
+    editLink: {
+      repo: 'prettier/plugin-pug',
+      branch: 'main',
+      dir: 'docs',
+      text: 'Suggest changes to this page',
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/prettier/plugin-pug' },
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-2022 Christopher Quadflieg',
+    },
+
     nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
+      { text: 'Guide', link: '/guide/' },
       {
         text: 'Configuration',
         items: [
@@ -32,17 +43,16 @@ export default defineConfig({
         ],
       },
     ],
+
     sidebar: {
       '/guide/': [
         {
           text: 'Guide',
-          collapsable: false,
-          children: [{ text: 'Getting Started', link: '/guide/' }],
+          items: [{ text: 'Getting Started', link: '/guide/' }],
         },
         {
           text: 'Configuration',
-          collapsable: false,
-          children: [
+          items: [
             {
               text: 'Standard Prettier overrides',
               link: '/guide/standard-prettier-overrides',
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,10 @@
+import Theme from 'vitepress/theme';
+import { h } from 'vue';
+import './styles/vars.css';
+
+export default {
+  ...Theme,
+  Layout() {
+    return h(Theme.Layout, null, {});
+  },
+};
