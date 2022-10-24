ecca60de5a468a7354f94a1f1b44c9dee4238145
Quadflieg , Christopher <christopher.quadflieg@dermalog.com>
2022-10-24 16:32:17 +0200

    Fix formatting

--- tests/issues/issue-417/issue-417.test.ts
+++ tests/issues/issue-417/issue-417.test.ts
@@ -1,8 +1,8 @@
 import { compareFiles } from 'tests/common';
-import { describe,expect,it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 describe('Issues', () => {
-  it('should add two empty piped lines before and after link tag.', () => {
+  it('should add two empty piped lines before and after link tag', () => {
     const { expected, actual } = compareFiles(__dirname);
     expect(actual).toBe(expected);
   });
