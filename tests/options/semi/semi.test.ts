ac42e257328559335af8219d3e891eb7fd433d2c
Shinigami92 <chrissi92@hotmail.de>
2023-01-14 13:36:31 +0100

    simplify test

--- tests/options/semi/semi.test.ts
+++ tests/options/semi/semi.test.ts
@@ -4,27 +4,23 @@ import type { CompareFilesOptions } from 'tests/common';
 import { compareFiles } from 'tests/common';
 import { describe, expect, it } from 'vitest';
 
-const getName: (v: boolean) => string = (v) => (v ? 'semi' : 'none');
-
 describe('semi handling', () => {
-  const matrix: Array<{ from: boolean; to: boolean }> = [
-    { from: true, to: true },
-    { from: true, to: false },
-    { from: false, to: true },
-    { from: false, to: false },
-  ];
-
-  for (const { from, to } of matrix) {
-    const _from: string = getName(from);
-    const _to: string = getName(to);
-
-    it(`it should format script tags with ${_from} inline code to ${_to}`, () => {
-      const fixturesPath: string = path.join(__dirname, `${_from}-${_to}`);
-      const options: CompareFilesOptions = { formatOptions: { semi: to } };
+  it.each([
+    ['semi', 'semi'],
+    ['semi', 'none'],
+    ['none', 'semi'],
+    ['none', 'none'],
+  ] as const)(
+    'should format script tags with %s inline code to %s',
+    (from, to) => {
+      const fixturesPath: string = path.join(__dirname, `${from}-${to}`);
+      const options: CompareFilesOptions = {
+        formatOptions: { semi: to === 'semi' },
+      };
 
       const { expected, actual } = compareFiles(fixturesPath, options);
 
       expect(actual).toBe(expected);
-    });
-  }
+    },
+  );
 });
