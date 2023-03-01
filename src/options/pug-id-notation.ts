0415df85db6956478d102a90cadf7d746e2b1c9a
fisker <lionkay@gmail.com>
2023-03-01 18:17:27 +0800

    Remove `since`

--- src/options/pug-id-notation.ts
+++ src/options/pug-id-notation.ts
@@ -3,7 +3,7 @@ import { CATEGORY_PUG } from './constants';
 
 /** Pug id notation. */
 export const PUG_ID_NOTATION: ChoiceSupportOption = {
-  since: '1.13.0',
+  // since: '1.13.0',
   category: CATEGORY_PUG,
   type: 'choice',
   default: 'literal',
