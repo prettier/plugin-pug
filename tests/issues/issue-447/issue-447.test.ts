60abaf4eb9ac35e3c801cbbc140e015259b97fdd
Shinigami92 <chrissi92@hotmail.de>
2023-06-02 20:22:43 +0200

    Handle whitespace in interpolations

---
+++ tests/issues/issue-447/issue-447.test.ts
@@ -0,0 +1,9 @@
+import { compareFiles } from 'tests/common';
+import { describe, expect, it } from 'vitest';
+
+describe('Issues', () => {
+  it('should handle whitespace in interpolations', () => {
+    const { expected, actual } = compareFiles(__dirname);
+    expect(actual).toBe(expected);
+  });
+});
