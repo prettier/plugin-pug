8ba5e363628465ca9a8c7a97674f965b139bde37
Shinigami92 <chrissi92@hotmail.de>
2023-09-24 01:03:27 +0200

    Rename vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,12 @@
-/// <reference types="vitest" />
 import { resolve } from 'node:path';
 import { URL, fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 const __dirname: string = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   resolve: {
     alias: {
-      // Needed for vitest
       src: resolve(__dirname, './src'),
       tests: resolve(__dirname, './tests'),
     },
