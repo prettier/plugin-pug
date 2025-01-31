d064af3a191ab6993f9aaca2a2a42cbbee671bef
Shinigami92 <chrissi92@hotmail.de>
2025-01-31 17:43:38 +0100

    Disable @typescript-eslint/no-misused-spread

--- eslint.config.ts
+++ eslint.config.ts
@@ -92,6 +92,7 @@ const config: ReturnType<typeof tseslint.config> = tseslint.config(
       '@typescript-eslint/member-ordering': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-misused-spread': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-parameter-properties': 'off',
       '@typescript-eslint/no-unnecessary-condition': 'warn',
