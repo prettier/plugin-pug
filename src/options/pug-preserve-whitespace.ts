5e7ee1b42cff18f15546ead4d693e9ba7bf2e991
Shinigami <chrissi92@hotmail.de>
2025-03-31 17:13:34 +0200

    Update src/options/pug-preserve-whitespace.ts

--- src/options/pug-preserve-whitespace.ts
+++ src/options/pug-preserve-whitespace.ts
@@ -6,7 +6,7 @@ export const PUG_PRESERVE_WHITESPACE: BooleanSupportOption = {
   // since: '3.3.0',
   category: CATEGORY_PUG,
   type: 'boolean',
-  // TODO @Shinigami92 2025-03-31: considering defaulting to false
+  // TODO @alexis-aquanty 2025-03-31: considering defaulting to false in v4
   default: true,
   description: 'Preserve additional whitespace in text.',
 };
