9eca25ca3837504bb9ede1abb41a93cc6af0254d
Davsem <64165880+DavidSemke@users.noreply.github.com>
2025-04-28 10:46:44 -0700

    Enhance quoting conversion on various pug statements (#578)

--- tests/options/singleQuote/single-false/single-false.test.ts
+++ tests/options/singleQuote/single-false/single-false.test.ts
@@ -10,5 +10,20 @@ describe('Options', () => {
 
       expect(actual).toBe(expected);
     });
+
+    it('should default to singleQuote:false', async () => {
+      const { expected, actual } = await compareFiles(import.meta.url);
+      expect(actual).toBe(expected);
+    });
+
+    it('should handle singleQuote:false + pugSingleQuote:false', async () => {
+      const { actual, expected } = await compareFiles(import.meta.url, {
+        formatOptions: {
+          singleQuote: false,
+          pugSingleQuote: false,
+        },
+      });
+      expect(actual).toBe(expected);
+    });
   });
 });
